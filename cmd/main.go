a2dda1325f7f2a3e8ba9eca880b390fef30630d1
iamnotrodger <rodger.retanal24@gmail.com>
2021-07-17 08:29:35 -0400

    [feature] CORS for the server

--- cmd/main.go
+++ cmd/main.go
@@ -10,13 +10,11 @@ import (
 	"github.com/iamnotrodger/art-house-api/internal/handler"
 	"github.com/iamnotrodger/art-house-api/internal/util"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
+	godotenv.Load()
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := util.MongoConnect(ctx)
@@ -41,11 +39,14 @@ func main() {
 	router.Handle("/api/artwork", handler.GetArtworks(db)).Methods("GET")
 	router.Handle("/api/artwork/{id}", handler.GetArtwork(db)).Methods("GET")
 	router.Handle("/api/artist", handler.GetArtists(db)).Methods("GET")
+	router.Handle("/api/artist/{id}", handler.GetArtist(db)).Methods("GET")
 	router.Handle("/api/artist/{id}/artwork", handler.GetArtistArtworks(db)).Methods("GET")
 	router.Handle("/api/exhibition", handler.GetExhibitions(db)).Methods("GET")
+	router.Handle("/api/exhibition/{id}", handler.GetExhibition(db)).Methods("GET")
 	router.Handle("/api/exhibition/{id}/artwork", handler.GetExhibitionArtworks(db)).Methods("GET")
 	router.Handle("/api/exhibition/{id}/artist", handler.GetExhibitionArtists(db)).Methods("GET")
 
+	server := cors.Default().Handler(router)
 	log.Println("API Started. Listening on", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(port, server))
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
+	github.com/rs/cors v1.8.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.3 // indirect
 )
