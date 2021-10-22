1ba903e3ff104a4d60393d65069740583bf3a833
Rodger Retanal <rodger.retanal24@gmail.com>
2021-10-21 23:10:40 -0400

    [feature] Created seed module
    This seeds the Mongo database for dev purposes.

---
+++ cmd/seed/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/iamnotrodger/art-house-api/internal/model"
+	"github.com/iamnotrodger/art-house-api/internal/util"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := util.MongoConnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	dbName, err := util.GetDatabaseName()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := client.Database(dbName)
+
+	artists, err := parseArtists("./cmd/seed/data/artists.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	artworks, err := parseArtworks("./cmd/seed/data/artworks.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exhibitions, err := parseExhibitions("./cmd/seed/data/exhibitions.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = model.InsertArtists(db, artists)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = model.InsertArtworks(db, artworks)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = model.InsertExhibitions(db, exhibitions)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parseArtists(filePath string) ([]model.Artist, error) {
+	var artists []model.Artist
+
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(file), &artists)
+	if err != nil {
+		return nil, err
+	}
+
+	return artists, nil
+}
+
+func parseArtworks(filePath string) ([]model.Artwork, error) {
+	var artworks []model.Artwork
+
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(file), &artworks)
+	if err != nil {
+		return nil, err
+	}
+
+	return artworks, nil
+}
+
+func parseExhibitions(filePath string) ([]model.Exhibition, error) {
+	var exhibitions []model.Exhibition
+
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(file), &exhibitions)
+	if err != nil {
+		return nil, err
+	}
+
+	return exhibitions, nil
+}
