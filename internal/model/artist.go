c1ab559d901a4ad39e1186d33b5859b1bd11b0b6
iamnotrodger <rodger.retanal24@gmail.com>
2021-12-13 21:38:16 -0500

    [update] Images on Artwork & Artist are now pointers

--- internal/model/artist.go
+++ internal/model/artist.go
@@ -1,7 +1,7 @@
 package model
 
 type Artist struct {
-	ID     string  `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name   string  `json:"name,omitempty" bson:"name,omitempty"`
-	Images []Image `json:"images,omitempty" bson:"images,omitempty"`
+	ID     string   `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name   string   `json:"name,omitempty" bson:"name,omitempty"`
+	Images []*Image `json:"images,omitempty" bson:"images,omitempty"`
 }
