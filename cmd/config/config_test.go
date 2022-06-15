fcd8a2504e91b02040b90f7b02017ddcf7300606
iamnotrodger <rodger.retanal24@gmail.com>
2022-06-14 21:51:05 -0400

    [update] `config` tests

--- cmd/config/config_test.go
+++ cmd/config/config_test.go
@@ -10,4 +10,20 @@ func TestGlobal(t *testing.T) {
 	assert.Equal(t, Global.Port, defaultPort)
 	assert.Equal(t, Global.MongoDBName, defaultMongoDBName)
 	assert.Equal(t, Global.MongoURI, defaultMongoURI)
+
+	assert.Equal(t, Global.RedisAddr, defaultRedisAddr)
+	assert.Equal(t, Global.RedisPassword, defaultRedisPassword)
+	assert.Equal(t, Global.RedisDb, defaultRedisDb)
+
+	assert.Equal(t, Global.ArtworkLimit, defaultArtworkLimit)
+	assert.Equal(t, Global.ArtworkLimitMin, defaultArtworkLimitMin)
+	assert.Equal(t, Global.ArtworkLimitMax, defaultArtworkLimitMax)
+
+	assert.Equal(t, Global.ArtistLimit, defaultArtistLimit)
+	assert.Equal(t, Global.ArtistLimitMin, defaultArtistLimitMin)
+	assert.Equal(t, Global.ArtistLimitMax, defaultArtistLimitMax)
+
+	assert.Equal(t, Global.ExhibitionLimit, defaultExhibitionLimit)
+	assert.Equal(t, Global.ExhibitionLimitMin, defaultExhibitionLimitMin)
+	assert.Equal(t, Global.ExhibitionLimitMax, defaultExhibitionLimitMax)
 }
