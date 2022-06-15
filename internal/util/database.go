d62eee14e817a7c2264d4d0b8a9b2dd7aca947e7
iamnotrodger <rodger.retanal24@gmail.com>
2022-06-14 21:51:29 -0400

    [feature] `GetRedisClient`

--- internal/util/database.go
+++ internal/util/database.go
@@ -3,12 +3,14 @@ package util
 import (
 	"context"
 
+	"github.com/go-redis/redis/v9"
+	"github.com/iamnotrodger/art-house-api/cmd/config"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func GetMongoClient(ctx context.Context, databaseURI string) (*mongo.Client, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(databaseURI))
+func GetMongoClient(ctx context.Context) (*mongo.Client, error) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(config.Global.MongoURI))
 	if err != nil {
 		return nil, err
 	}
@@ -18,4 +20,12 @@ func GetMongoClient(ctx context.Context, databaseURI string) (*mongo.Client, err
 	}
 
 	return client, nil
+}
+
+func GetRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     config.Global.RedisAddr,
+		Password: config.Global.RedisPassword,
+		DB:       config.Global.RedisDb,
+	})
 }
